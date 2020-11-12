46248cb97b36ad06a111f713b6084b0054daa5cc
Gustavo F. Silva <gustafsilva.dev@gmail.com>
2020-11-11 21:28:53 -0300

    Finishing the Accordion widget from App Widgets.

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,18 +1,32 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import PropTypes from "prop-types";
 
 function Accordion({ items }) {
-    const renderedItems = items.map(item => (
-        <Fragment key={item.title}>
-            <div className="title active">
-                <i className="dropdown icon"></i>
-                {item.title}
-            </div>
-            <div className="content active">
-                <p>{item.content}</p>
-            </div>
-        </Fragment>
-    ))
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index);
+    }
+
+    const renderedItems = items.map((item, index) => {
+        const active = index === activeIndex ? 'active' : '';
+
+
+        return (
+            <Fragment key={item.title}>
+                <div
+                    className={`title ${active}`}
+                    onClick={() => onTitleClick(index)}
+                >
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className={`content ${active}`}>
+                    <p>{item.content}</p>
+                </div>
+            </Fragment>
+        );
+    })
 
     return (
         <div className="ui styled accordion">
